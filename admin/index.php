dbd8e03db1c528932833ee0eca0556351a282d65
Tran Duc Luong <haychotoibietmatkhau@gmail.com>
2024-05-05 17:13:37 +0700

    push folder

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+ob_start();
+if (isset($_SESSION['role']) && ($_SESSION['role'] == 1)) {
+    include "../model/connectdb.php";
+}
+else{
+    header('location: login.php');
+}
+?>
